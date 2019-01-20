fbdb5acffbe22ecb3f8c46185e88bd86357b68d8
Max Kunowski <>
2019-01-20 09:46:58 -0800

    fix(visualizer): timing is now based on recorded time markers for each iteration

--- src/visualizer/visualizer.ts
+++ src/visualizer/visualizer.ts
@@ -1,11 +1,15 @@
 import * as config from '../../config';
 import { Storage } from '../utils';
 
+interface Pitches {
+  s: number;
+  d: number[];
+}
+
 class Visualizer {
   private isRunning: boolean;
   private storage: Storage;
   private uidProgressKey: string;
-  private uidTrackDurationKey: string;
   private uidTrackPitchKey: string;
 
   private nodeCollection: HTMLCollection;
@@ -16,13 +20,12 @@ class Visualizer {
     this.isRunning = false;
     this.storage = new Storage();
     this.uidProgressKey = config.UID_PROGRESS_KEY;
-    this.uidTrackDurationKey = config.UID_TRACK_DURATION_KEY;
     this.uidTrackPitchKey = config.UID_TRACK_PITCH_KEY;
 
     this.nodeCollection = document.getElementsByClassName('node');
   }
 
-  private getSyncIndex(inputArray: [], prog: any): number {
+  private getSyncIndex(inputArray: any[], prog: any): number {
     return inputArray.reduce((prev: number, curr: any, idx: number, ref: any) =>
       Math.abs(curr.s - prog) < Math.abs(ref[prev].s - prog) ? idx : prev, 0)
   }
@@ -31,7 +34,7 @@ class Visualizer {
     this.stop();
     this.isRunning = true;
 
-    const pitches = this.storage.get(this.uidTrackPitchKey) as [] || [];
+    const pitches = this.storage.get(this.uidTrackPitchKey) as Pitches[] || [];
 
     if (!pitches.length) {
       this.stop();
@@ -42,21 +45,11 @@ class Visualizer {
       return void 0;
     }
 
-    const runTime: number = +this.storage.get(this.uidTrackDurationKey) / 1000 / 60;
-    const intervalTimer: number = Math.round((this.storage.get(this.uidTrackPitchKey) as [] || []).length / runTime);
-    let prog: any = this.storage.get(this.uidProgressKey);
+    const prog: any = this.storage.get(this.uidProgressKey);
     let tick: number = this.getSyncIndex(pitches, prog);
-    let lastProg: number = 0;
 
-    this.interval = setInterval(() => {
-      prog = this.storage.get(this.uidProgressKey);
-
-      if (lastProg !== prog) {
-        lastProg = prog;
-        tick = this.getSyncIndex(pitches, prog);
-      }
-
-      const currentPitchRange: [] = pitches[tick] ? (pitches[tick] as {s: number, d: []}).d : [];
+    const playerSync = () => {
+      const currentPitchRange: number[] = pitches[tick] ? pitches[tick].d : [];
 
       for (let i = 0, ii = this.nodeCollection.length; i < ii; ++i) {
         for ( let z = 0, zz = this.nodeCollection[i].children.length; z < zz; ++z) {
@@ -64,12 +57,23 @@ class Visualizer {
         }
       }
 
+      this.interval = setTimeout((self: any) => {
+        if (pitches[tick + 1]) {
+          playerSync();
+          return void 0;
+        }
+
+        clearTimeout(self.interval);
+      }, ((pitches[tick + 1].s - pitches[tick].s) * 1000), this);
+
       tick += 1;
-    }, intervalTimer);
+    }
+
+    playerSync();
   }
 
   public stop(): void {
-    clearInterval(this.interval);
+    clearTimeout(this.interval);
     this.isRunning = false;
   }
 
