d1a0be81ff0e6a4418c77707956f78da0b96905d
Max Schulz-Kunowski <max.schulz-kunowski@ey.com>
2019-01-25 12:40:02 -0800

    chore(cleanup): minor refactor for readability

--- src/services/audioAnalysis.service.ts
+++ src/services/audioAnalysis.service.ts
@@ -107,26 +107,16 @@ class AudioAnalysisService {
    */
   private getPeakValue(val: number): number {
     switch (true) {
-      case val > 501.85: return 10;
-      case val > 240.618: return 9;
-      case val > 116.103: return 8;
-      case val > 55.34: return 7;
-      case val > 27.006: return 6;
-      case val > 12.593: return 5;
-      case val > 6.406: return 4;
-      case val > 2.75: return 3;
-      case val > 1.625: return 2;
-      case val > 0.5: return 1;
-      case val < -501.85: return 10;
-      case val < -240.618: return 9;
-      case val < -116.103: return 8;
-      case val < -55.34: return 7;
-      case val < -27.006: return 6;
-      case val < -12.593: return 5;
-      case val < -6.406: return 4;
-      case val < -2.75: return 3;
-      case val < -1.625: return 2;
-      case val < -0.5: return 1;
+      case val > 501.85 || val < -501.85: return 10;
+      case val > 240.618 || val < -240.618: return 9;
+      case val > 116.103 || val < -116.103: return 8;
+      case val > 55.34 || val < -55.34: return 7;
+      case val > 27.006 || val < -27.006: return 6;
+      case val > 12.593 || val < -12.593: return 5;
+      case val > 6.406 || val < -6.406: return 4;
+      case val > 2.75 || val < -2.75: return 3;
+      case val > 1.625 || val < -1.625: return 2;
+      case val > 0.5 || val < -0.5: return 1;
       default: return 0;
     }
   }
